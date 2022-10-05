7209ff319d2818768df4baafd8156e2225997459
Duncan Cheruiyot Langat <duncan.langat@student.moringaschool.com>
2022-10-05 07:46:38 +0300

    add some paragraph about me

--- src/components/About.js
+++ src/components/About.js
@@ -5,8 +5,8 @@ function About() {
     <div className='about'>
       <h1>About Me</h1>
       <p>My name is Duncan Cheruiyot Langat, I am studying Software Engineering specializing in fullstack web Development.
-        My passions for learning and gaining   </p>
-
+        My passions for learning and gaining.I am very ambitious Junior Web Developer looking <br></br> in established IT company with the opportunity to work withe latest technologies<br></br> on challenging and diverse projects. </p>
+       <p> I'm quietly confident,naturally curious,and perpetually woriking on improvig my skills as a web developer.</p>
     </div>
   )
 }
