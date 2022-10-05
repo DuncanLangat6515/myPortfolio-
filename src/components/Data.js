960377a50ff2e6a54061f3714a830334d68aae4b
Duncan Cheruiyot Langat <duncan.langat@student.moringaschool.com>
2022-10-05 18:03:24 +0300

    declare variable

--- src/components/Data.js
+++ src/components/Data.js
@@ -2,4 +2,10 @@ const user={
     name:"Duncan",
     url: "https://scontent.fnbo2-1.fna.fbcdn.net/v/t1.6435-9/141516148_2795056727409617_8181471895462743127_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=e3f864&_nc_ohc=8FNQ9_2O8_AAX8tocsG&_nc_ht=scontent.fnbo2-1.fna&oh=00_AT_NpBE7bvF8Gw8AxJcAuHMLR2fi0wKq1JJ0eeWsWifavw&oe=6360D270"
 }
-export {user}
+
+const code={
+    image:"https://images.pexels.com/photos/1181263/pexels-photo-1181263.jpeg?auto=compress&cs=tinysrgb&w=1600"
+}
+
+
+export {user,code}
