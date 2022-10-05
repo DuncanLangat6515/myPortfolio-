3f3bfd447dd74667ed039caf097354e8a54a7943
Duncan Cheruiyot Langat <duncan.langat@student.moringaschool.com>
2022-10-05 18:06:54 +0300

    fetch data from api

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 function Project() {
-  return (
-    <div>Project</div>
-  )
+  const [projects, setProjects] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3000/projects")
+      .then((res) => res.json())
+      .then((data) => {
+        setProjects(data);
+      });
+  }, []);
+  const proDetails = projects.map((everyproject) => (
+    <>
+      <div className="imageCard">
+        <h1>{everyproject.name}</h1>
+        <img src={everyproject.image } alt="oops"/>
+      </div>
+    </>
+  ));
+  return <div className="card">
+    {proDetails}
+  </div>;
 }
 
-export default Project
+export default Project;
