5c9e31f14d6be7f665b507b8152907b45808dc08
Duncan Cheruiyot Langat <duncan.langat@student.moringaschool.com>
2022-10-05 18:06:12 +0300

    make changes in button

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import {user} from "./Data"
+import { Link } from 'react-router-dom'
+
 
 function Home(){
   return(
@@ -12,12 +14,14 @@ function Home(){
         <h1>Hey I am <br></br> Duncan Langat  </h1>
         <h2>Junior Frontend Developer/ JavaScript Expert/ Content Creator </h2>
        </div>
-    </div>
-    
-    
-
+</div>
+    <Link to='/contact' className='button'>HIRE ME</Link>
     </>
   )
 }
 
 export default Home 
+
+
+
+
